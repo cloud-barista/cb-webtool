de7cb1662ab0afaef26cb7a4feb1a680d87e7967
jazmandorf <jazmandorf@gmail.com>
2019-11-07 20:49:06 +0900

    modified

--- src/test/requestTest.go
+++ src/test/requestTest.go
@@ -11,12 +11,15 @@ func main() {
 	//url := "http://localhost:1234/ns"
 	//nsInfo := service.NSInfo{}
 
-	nsList := service.GetNSList()
-	for _, item := range nsList {
+	// nsList := service.GetNSList()
+	// for _, item := range nsList {
 
-		fmt.Println("ID : ", item.ID)
+	// 	fmt.Println("ID : ", item.ID)
 
-	}
-	fmt.Println(nsList)
+	// }
+	// fmt.Println(nsList)
+
+	info := service.GetMCISList("12345")
+	fmt.Println(info)
 
 }
