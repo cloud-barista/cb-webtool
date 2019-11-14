2e76b3f2798305e816b128ca6b15992e56f80a82
mzcsc <jaz@mzcscui-MacBookPro.local>
2019-11-14 16:49:00 +0900

    modified

--- src/static/assets/js/common.js
+++ src/static/assets/js/common.js
@@ -1,23 +1,4 @@
-/*
-@author https://github.com/macek/jquery-serialize-object
-*/
-$.fn.serializeObject = function() { 
-    var obj = null; 
-    try { 
-        if(this[0].tagName && this[0].tagName.toUpperCase() == "FORM" ) {
-             var arr = this.serializeArray(); 
-             if(arr){ 
-                 obj = {}; 
-                 $.each(arr, function() { 
-                     obj[this.name] = this.value; 
-                    }); 
-                }
-            } 
-        }catch(e) { 
-            alert(e.message); 
-        }finally {
+funtcion requestAjax( url, method,data){
 
-        } 
-        return obj; 
-    }
+}
 
