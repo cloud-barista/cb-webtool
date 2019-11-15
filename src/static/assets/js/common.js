6411c5f1c489f90642c22ed770a3cf99f2fbd50d
mzcsc <jaz@mzcscui-MacBookPro.local>
2019-11-15 23:24:30 +0900

    modified

--- src/static/assets/js/common.js
+++ src/static/assets/js/common.js
@@ -1,17 +1,17 @@
-funtcion requestAjax(url, method, data){
-    console.log("Request URL : ",url)
-    var met = method.toLowerCase
-    $.ajax({
-        url : url,
-        type: method,
-        data: data
+// funtcion requestAjax(url, method, data){
+//     console.log("Request URL : ",url)
+//     var met = method.toLowerCase
+//     $.ajax({
+//         url : url,
+//         type: method,
+//         data: data
 
-    }).then(function(result){
-        console.log(result)
-    })
-}
+//     }).then(function(result){
+//         console.log(result)
+//     })
+// }
 
-function requestAxios()
+
 
 function getNameSpace(){
     var url = CommonURL+"/ns"
