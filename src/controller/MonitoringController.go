5c539304a6b06400d20e3cdf50f1343e8ec7cb05
jazmandorf <jazmandorf@gmail.com>
2020-05-15 20:51:32 +0900

    modified cb-webtool

--- src/controller/MonitoringController.go
+++ src/controller/MonitoringController.go
@@ -21,3 +21,19 @@ func MornitoringListForm(c echo.Context) error {
 	//return c.Render(http.StatusOK, "MCISlist.html", nil)
 	return c.Redirect(http.StatusTemporaryRedirect, "/login")
 }
+
+func AgentRegForm(c echo.Context) error {
+	comURL := GetCommonURL()
+	if loginInfo := CallLoginInfo(c); loginInfo.Username != "" {
+		namespace := GetNameSpaceToString(c)
+		return c.Render(http.StatusOK, "InstallAgent.html", map[string]interface{}{
+			"LoginInfo": loginInfo,
+			"NameSpace": namespace,
+			"comURL":    comURL,
+		})
+
+	}
+
+	//return c.Render(http.StatusOK, "MCISlist.html", nil)
+	return c.Redirect(http.StatusTemporaryRedirect, "/login")
+}
