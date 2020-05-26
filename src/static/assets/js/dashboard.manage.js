0a00600cbf31a360cf7793b598d5b64d3dc68489
devunet <devunet@mz.co.kr>
2020-05-26 20:23:45 +0900

    Dash보드 클릭 & 맵 높이 수정

--- src/static/assets/js/dashboard.manage.js
+++ src/static/assets/js/dashboard.manage.js
@@ -104,7 +104,7 @@ function short_desc(str){
             if(count == 1){
 
             }
-            html += '<tr id="tr_id_'+count+'" >'
+            html += '<tr id="tr_id_'+count+'" class="clickable-row">'
              +'<td class="text-center">'
              +'<div class="form-input">'
              +'<span class="input">'
@@ -238,7 +238,7 @@ function show_vmList(mcis_id,map){
                         if(count == 1){
             
                         }
-                        html += '<tr id="tr_id_'+count+'" >'
+                        html += '<tr id="tr_id_'+count+'"  class="clickable-row">'
                         +'<td class="text-center">'
                         +'<div class="form-input">'
                         +'<span class="input">'
