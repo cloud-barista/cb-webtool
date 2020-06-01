7958f472dd45413455eff9b28e479b3f357d5e3e
jazmandorf <jazmandorf@gmail.com>
2020-06-01 14:48:05 +0900

    modified cb-webtool

--- src/controller/CommonHandler.go
+++ src/controller/CommonHandler.go
@@ -123,8 +123,8 @@ func CallLoginInfo(c echo.Context) LoginInfo {
 
 	result := getObj.(map[string]string)
 	loginInfo := LoginInfo{
-		Username: "admin",
-		//Username:  result["username"],
+		//Username: "admin",
+		Username:  result["username"],
 		NameSpace: result["namespace"],
 	}
 	getNs, ok := store.Get("namespace")
