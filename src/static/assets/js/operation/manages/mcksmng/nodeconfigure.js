6cf86bdc93d851bfbbfe904dbb6d01b0d234a366
nohis1 <nohis1@knou.ac.kr>
2021-05-17 23:52:53 +0900

    node 작업중

--- src/static/assets/js/operation/manages/mcksmng/nodeconfigure.js
+++ src/static/assets/js/operation/manages/mcksmng/nodeconfigure.js
@@ -39,3 +39,75 @@ function getSpecInfo(configName, targetObjId){
         $("#" + targetObjId).append(html);        
     })
 }
+
+// mcks , node deploy
+// 우선 mcks 부터
+function deploy_btn(){
+    var mcksName = $("#mcks_name").val();
+
+    var kubernatesNetworkCni = $("#kubernatesNetworkCni").val();
+    var kubernatesPodCidr = $("#kubernatesPodCidr").val();
+    var kubernatesServiceCidr = $("#kubernatesServiceCidr").val();
+    var kubernatesServiceDnsDomain = $("#kubernatesServiceDnsDomain").val();
+    
+    var controlPlaneConnectionLength = $("input[name='controlPlaneConnectionName']").length;
+    console.log("controlPlaneConnectionLength1 " + controlPlaneConnectionLength)
+    var controlPlaneConnectionData = new Array(controlPlaneConnectionLength);
+    var controlPlaneCountData = new Array(controlPlaneConnectionLength);
+    var controlPlaneSpecIdData = new Array(controlPlaneConnectionLength);
+    for(var i=0; i<controlPlaneConnectionLength; i++){                          
+        controlPlaneConnectionData[i] = $("input[name='controlPlaneConnectionName']")[i].value;
+        controlPlaneCountData[i] = $("input[name='controlPlaneCount']")[i].value;
+        controlPlaneSpecIdData[i] = $("input[name='controlPlaneSpecId']")[i].value;
+    }
+    
+    var workerConnectionNameLength = $("input[name='workerConnectionName']").length;
+    console.log("workerConnectionNameLength1 " + workerConnectionNameLength)
+    var workerConnectionData = new Array(workerConnectionNameLength);
+    var workerCountData = new Array(workerConnectionNameLength);
+    var workerSpecIdData = new Array(workerConnectionNameLength);
+    for(var i=0; i<workerConnectionNameLength; i++){                          
+        workerConnectionData[i] = $("input[name='workerConnectionName']")[i].value;
+        workerCountData[i] = $("input[name='controlPlaneCount']")[i].value;
+        workerSpecIdData[i] = $("input[name='workerSpecId']")[i].value;
+    }
+    
+    var new_obj = {}
+    // mcis 생성이므로 mcisID가 없음
+    new_obj['name'] = mcksName
+    
+    var new_kubernetes = {}
+    new_kubernetes['networkCni'] = kubernatesNetworkCni;
+    new_kubernetes['podCidr'] = kubernatesPodCidr;
+    new_kubernetes['serviceCidr'] = kubernatesServiceCidr;
+    new_kubernetes['serviceDnsDomain'] = kubernatesServiceDnsDomain;
+
+    new_obj['config'] = new_kubernetes
+    var controlPlanes = new Array(controlPlaneConnectionLength);
+    console.log("controlPlaneConnectionLength " + controlPlaneConnectionLength)
+    for(var i=0; i<controlPlaneConnectionLength; i++){
+        console.log("controlPlane " + i)
+        var new_controlPlane = {}
+        new_controlPlane['connection'] = controlPlaneConnectionData[i];
+        new_controlPlane['count'] = controlPlaneCountData[i]
+        new_controlPlane['spec'] = controlPlaneSpecIdData[i]
+        controlPlanes[i] = new_controlPlane
+    }
+    new_obj['controlPlane'] = controlPlanes;
+
+    var workers = new Array();
+    $("input[name='workerCount']").each(function (i) {
+        var new_worker = {}
+        console.log($("select[name='workerConnectionName']").eq(i));
+        new_worker['connection'] = $("select[name='workerConnectionName']").eq(i).attr("value");
+        new_worker['count'] = $("input[name='workerCount']").eq(i).attr("value")
+        new_worker['spec'] = $("select[name='workerSpecId']").eq(i).attr("value")
+        workers[i] = new_worker
+        console.log( i + "번째  : " );
+        console.log( new_worker);
+   });
+
+
+    new_obj['worker'] = workers;
+    console.log(new_obj);
+}
