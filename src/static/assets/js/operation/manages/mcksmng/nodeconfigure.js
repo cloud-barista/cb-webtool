d3480b8cc3a2f01dfa2c80bbd520c1c938376cb8
dogfootman <yhnoh@megazone.com>
2021-05-18 10:37:15 +0900

    form param 전송 보완

--- src/static/assets/js/operation/manages/mcksmng/nodeconfigure.js
+++ src/static/assets/js/operation/manages/mcksmng/nodeconfigure.js
@@ -50,28 +50,33 @@ function deploy_btn(){
     var kubernatesServiceCidr = $("#kubernatesServiceCidr").val();
     var kubernatesServiceDnsDomain = $("#kubernatesServiceDnsDomain").val();
     
-    var controlPlaneConnectionLength = $("input[name='controlPlaneConnectionName']").length;
-    console.log("controlPlaneConnectionLength1 " + controlPlaneConnectionLength)
-    var controlPlaneConnectionData = new Array(controlPlaneConnectionLength);
-    var controlPlaneCountData = new Array(controlPlaneConnectionLength);
-    var controlPlaneSpecIdData = new Array(controlPlaneConnectionLength);
-    for(var i=0; i<controlPlaneConnectionLength; i++){                          
-        controlPlaneConnectionData[i] = $("input[name='controlPlaneConnectionName']")[i].value;
+    var controlPlaneLength = $("input[name='controlPlaneCount']").length;
+    console.log("controlPlaneLength1 " + controlPlaneLength)
+    var controlPlaneConnectionData = new Array(controlPlaneLength);
+    var controlPlaneCountData = new Array(controlPlaneLength);
+    var controlPlaneSpecIdData = new Array(controlPlaneLength);
+    for(var i=0; i<controlPlaneLength; i++){                          
+        controlPlaneConnectionData[i] = $("select[name='controlPlaneConnectionName']")[i].value;
         controlPlaneCountData[i] = $("input[name='controlPlaneCount']")[i].value;
-        controlPlaneSpecIdData[i] = $("input[name='controlPlaneSpecId']")[i].value;
+        controlPlaneSpecIdData[i] = $("select[name='controlPlaneSpecId']")[i].value;
     }
+    console.log(controlPlaneConnectionData)
+    console.log(controlPlaneCountData)
+    console.log(controlPlaneSpecIdData)
     
-    var workerConnectionNameLength = $("input[name='workerConnectionName']").length;
-    console.log("workerConnectionNameLength1 " + workerConnectionNameLength)
-    var workerConnectionData = new Array(workerConnectionNameLength);
-    var workerCountData = new Array(workerConnectionNameLength);
-    var workerSpecIdData = new Array(workerConnectionNameLength);
-    for(var i=0; i<workerConnectionNameLength; i++){                          
-        workerConnectionData[i] = $("input[name='workerConnectionName']")[i].value;
+    var workerCountLength = $("input[name='workerCount']").length;
+    console.log("workerCountLength1 " + workerCountLength)
+    var workerConnectionData = new Array(workerCountLength);
+    var workerCountData = new Array(workerCountLength);
+    var workerSpecIdData = new Array(workerCountLength);
+    for(var i=0; i<workerCountLength; i++){                          
+        workerConnectionData[i] = $("select[name='workerConnectionName']")[i].value;
         workerCountData[i] = $("input[name='controlPlaneCount']")[i].value;
-        workerSpecIdData[i] = $("input[name='workerSpecId']")[i].value;
+        workerSpecIdData[i] = $("select[name='workerSpecId']")[i].value;
     }
-    
+    console.log(workerConnectionData)
+    console.log(workerCountData)
+    console.log(workerSpecIdData)
     var new_obj = {}
     // mcis 생성이므로 mcisID가 없음
     new_obj['name'] = mcksName
@@ -83,9 +88,9 @@ function deploy_btn(){
     new_kubernetes['serviceDnsDomain'] = kubernatesServiceDnsDomain;
 
     new_obj['config'] = new_kubernetes
-    var controlPlanes = new Array(controlPlaneConnectionLength);
-    console.log("controlPlaneConnectionLength " + controlPlaneConnectionLength)
-    for(var i=0; i<controlPlaneConnectionLength; i++){
+    var controlPlanes = new Array(controlPlaneLength);
+    console.log("controlPlaneConnectionLength " + controlPlaneLength)
+    for(var i=0; i<controlPlaneLength; i++){
         console.log("controlPlane " + i)
         var new_controlPlane = {}
         new_controlPlane['connection'] = controlPlaneConnectionData[i];
@@ -95,19 +100,64 @@ function deploy_btn(){
     }
     new_obj['controlPlane'] = controlPlanes;
 
-    var workers = new Array();
-    $("input[name='workerCount']").each(function (i) {
+    var workers = new Array(workerCountLength);
+    for(var i=0; i<workerCountLength; i++){
+        console.log("workerCountLength " + i)
         var new_worker = {}
-        console.log($("select[name='workerConnectionName']").eq(i));
-        new_worker['connection'] = $("select[name='workerConnectionName']").eq(i).attr("value");
-        new_worker['count'] = $("input[name='workerCount']").eq(i).attr("value")
-        new_worker['spec'] = $("select[name='workerSpecId']").eq(i).attr("value")
+        new_worker['connection'] = workerConnectionData[i];
+        new_worker['count'] = workerCountData[i]
+        new_worker['spec'] = workerSpecIdData[i]
         workers[i] = new_worker
-        console.log( i + "번째  : " );
-        console.log( new_worker);
-   });
-
-
+    }
     new_obj['worker'] = workers;
+//     $("input[name='workerCount']").each(function (i) {
+//         var new_worker = {}
+//         console.log($("select[name='workerConnectionName']").eq(i));
+//         new_worker['connection'] = $("select[name='workerConnectionName']").eq(i).attr("value");
+//         new_worker['count'] = $("input[name='workerCount']").eq(i).attr("value")
+//         new_worker['spec'] = $("select[name='workerSpecId']").eq(i).attr("value")
+//         workers[i] = new_worker
+//         console.log( i + "번째  : " );
+//         console.log( new_worker);
+//    });
+   
     console.log(new_obj);
+
+    try{
+        var url = "/operation/manages/mcksmng/" + mcis_id +"/vm/reg/proc"
+        axios.post(url,new_obj,{
+            headers :{
+                },
+        }).then(result=>{
+            console.log("MCIR VM Register data : ",result);
+            console.log("Result Status : ",result.status); 
+
+            var statusCode = result.data.status;
+            var message = result.data.message;
+            console.log("Result Status : ",statusCode); 
+            console.log("Result message : ",message); 
+
+            if(result.status == 201 || result.status == 200){
+                vmCreateCallback(new_obj.name, "Success")
+            //     commonAlert("Register Success")
+            //     // location.href = "/Manage/MCIS/list";
+            //     // $('#loadingContainer').show();
+            //     // location.href = "/operation/manages/mcis/mngform/"
+            //     var targetUrl = "/operation/manages/mcis/mngform"
+            //     changePage(targetUrl)
+            }else{
+                vmCreateCallback(new_obj.name, message)    
+            //     commonAlert("Register Fail")
+            //     //location.reload(true);
+            }
+        }).catch((error) => {
+            // console.warn(error);
+            console.log(error.response)
+            var errorMessage = error.response.data.error;
+            // commonErrorAlert(statusCode, errorMessage) 
+            vmCreateCallback(new_obj.name, errorMessage)
+        })
+    }finally{
+        
+    }
 }
