8b6e13b3f742a6a8749e7349558b9e0c92efccec
mzc-ljhoon <ljhoon@mz.co.kr>
2021-06-09 08:12:11 +0900

    Alert Event List 적용

---
+++ src/model/dragonfly/VmMonitoringAlertLog.go
@@ -0,0 +1,9 @@
+package dragonfly
+
+//
+type VmMonitoringAlertLog struct {
+	Id      string `json:"id"`
+	Time    string `json:"time"`
+	Level   string `json:"level"`
+	Message string `json:"message"`
+}
