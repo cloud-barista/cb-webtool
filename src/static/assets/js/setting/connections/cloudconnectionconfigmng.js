696ed4c9aac8cc9c10afe768742bd6afe02e35bf
dogfootman <yhnoh@megazone.com>
2021-06-11 17:53:23 +0900

    Credential key 입력 편의 추가

--- src/static/assets/js/setting/connections/cloudconnectionconfigmng.js
+++ src/static/assets/js/setting/connections/cloudconnectionconfigmng.js
@@ -941,3 +941,69 @@ function deleteDriver(){
         });
 }
 
+
+function selProvider(providerName){
+    
+    var liKey0 = $("#liKey0")
+    var liValue0 = $("#liValue0")
+    var liKey1 = $("#liKey1")
+    var liValue1 = $("#liValue1")
+    var liKey2 = $("#liKey2")
+    var liValue2 = $("#liValue2")
+    var liKey3 = $("#liKey3")
+    var liValue3 = $("#liValue3")
+    var key0 = $("#CredentialModalKey0")
+    var val0 = $("#CredentialModalValue0")
+    var key1 = $("#CredentialModalKey1")
+    var val1 = $("#CredentialModalValue1")
+    var key2 = $("#CredentialModalKey2")
+    var val2 = $("#CredentialModalValue2")
+    var key3 = $("#CredentialModalKey3")
+    var val3 = $("#CredentialModalValue3")
+
+    // 초기화 하고 시작
+    key0.val("");
+    val0.val("");
+    key1.val("");
+    val1.val("");
+    key2.val("");
+    val2.val("");
+    key3.val("");
+    val3.val("");    
+    
+    liKey0.css("display", "")
+    liValue0.css("display", "")
+    liKey1.css("display", "")
+    liValue1.css("display", "")
+    liKey2.css("display", "")
+    liValue2.css("display", "")
+    liKey3.css("display", "")
+    liValue3.css("display", "")        
+        
+    if( providerName == "AWS"){
+        key0.val("ClientId");
+        key1.val("ClientSecret");
+        
+        liKey2.css("display", "none")
+        liValue2.css("display", "none")
+        liKey3.css("display", "none")
+        liValue3.css("display", "none")        
+    }else if ( providerName == "GCP"){
+        key0.val("ClientEmail");
+        key1.val("ProjectID");
+        key2.val("PrivateKey");        
+        
+        liKey2.css("display", "")
+        liValue2.css("display", "")
+        liKey3.css("display", "none")
+        liValue3.css("display", "none")        
+    }else if ( providerName == "ALIBABA"){
+        key0.val("ClientId");
+        key1.val("ClientSecret");
+        
+        liKey2.css("display", "none")
+        liValue2.css("display", "none")
+        liKey3.css("display", "none")
+        liValue3.css("display", "none")        
+    }
+}
