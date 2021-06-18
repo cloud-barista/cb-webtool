03fc45f0e2971b926e0e0ee4db36e1357ebb38dc
dogfootman <yhnoh@megazone.com>
2021-06-18 15:51:31 +0900

    mcks는 인스턴스 상태가 없으므로 default값은 running으로

--- src/static/assets/js/operation/manages/mcksmng/mcksmng.js
+++ src/static/assets/js/operation/manages/mcksmng/mcksmng.js
@@ -60,7 +60,7 @@ function showServerListAndStatusArea(uid, mcksIndex){
             var nodeID = $("#mcksNodeUID_" + thisMcksIndex + "_" + nodeIndexOfMcks).val();
             var nodeName = $("#mcksNodeName_" + thisMcksIndex + "_" + nodeIndexOfMcks).val();
                 
-            nodeStatusIcon ="bgbox_g"
+            nodeStatusIcon ="bgbox_b"
             // node 목록 표시
             mcksNodes += '<li class="sel_cr ' + nodeStatusIcon + '"><a href="javascript:void(0);" onclick="nodeDetailInfo(\''+thisMcksIndex+'\',\''+nodeIndexOfMcks+'\')"><span class="txt">'+nodeName+'</span></a></li>';
         }
