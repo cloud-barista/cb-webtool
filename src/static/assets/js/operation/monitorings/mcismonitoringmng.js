9e1286939c2e5ba5f8f1c1fa6bffba22657297a5
dogfootman <idogfootman@gmail.com>
2021-11-17 16:56:36 +0900

    Monitoring - MCIS 페이지 로딩 방식 변경.

--- src/static/assets/js/operation/monitorings/mcismonitoringmng.js
+++ src/static/assets/js/operation/monitorings/mcismonitoringmng.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    getCommonMcisList("mcismonitoringmng", true, "status")
+    getCommonMcisList("mcismonitoringmng", true, '', "id")
     resizeContent();
 });
 $(window).resize(function() {
@@ -34,21 +34,29 @@ function getMcisListCallbackSuccess(caller, mcisList){
 
     // MCIS Status
     var addMcis = "";
+    addMcis += '<option>Choose a Target MCIS for Monitoring</option>';
     if(!isEmpty(mcisList) && mcisList.length > 0 ){
-        var initMcis = mcisList[0].id
-
+        var initMcis = $("#init_mcis").val();
+        console.log(mcisList)
         for(var i in mcisList){
-            if(i == 0 ){
-                addMcis +='<option value="'+mcisList[i].id+'"  selected>'+mcisList[i].name+"|"+mcisList[i].status+"|"+mcisList[i].description
-            }else{
-                addMcis +='<option value="'+mcisList[i].id+'" >'+mcisList[i].name+"|"+mcisList[i].status+"|"+mcisList[i].description
-            }
+            // if(i == 0 ){
+            //     addMcis +='<option value="'+mcisList[i].id+'"  selected>'+mcisList[i].name+"|"+mcisList[i].status+"|"+mcisList[i].description
+            // }else{
+            //     addMcis +='<option value="'+mcisList[i].id+'" >'+mcisList[i].name+"|"+mcisList[i].status+"|"+mcisList[i].description
+            // }
+            addMcis +='<option value="'+mcisList[i]+'">'+mcisList[i] + '</option>';
         }
         $("#mcisList").empty()
         $("#mcisList").append(addMcis)
         if (initMcis != "") {
-            selectMonitoringMcis(initMcis)
+
+        }else{
+            initMcis = mcisList[0]
         }
+
+        // id="mcisList"
+        $("#mcisList").val(initMcis).prop("selected", true);
+        selectMonitoringMcis(initMcis)
     }else{
         var addMcis = "";
 
@@ -103,97 +111,184 @@ function selectMonitoringMcis(mcisId){
     // var nsid = NAMESPACE
     // var url = "{{ .comURL.TumbleBugURL}}"+"/ns/"+nsid+"/mcis/"+mcis_id;
     // var apiInfo = "{{ .apiInfo}}";
-    var url = "/operation/manages/mcismng/" + mcisId
-    axios.get(url,{
-        headers:{
-            // 'Authorization': apiInfo,
-            'Content-Type' : "application/json"
-        }
-    }).then(result=>{
-        console.log("selectMonitoringMcis result")
-        var mcis = result.data.McisInfo
-        console.log(mcis)
-        var vms = mcis.vm
-        console.log(vms)
-        var vm_badge ="";
-        var vm_options = "";
-        var init_vm = vms[0].id
-        if(vms){
-            vm_len = vms.length
-            for(var o in vms){
-                var vm_status = vms[o].status
-                vm_options +='<option value="'+vms[o].id+'">'+vms[o].name+'|'+vms[o].status+'|'+vms[o].description
 
-                var vmStatusIcon = "bgbox_b";
-                if(vm_status == "Running"){
-                    vmStatusIcon = "bgbox_b";
-                }else if(vm_status == "include" ){
-                    vmStatusIcon = "bgbox_g";                    
-                }else if(vm_status == "Suspended"){
-                    vmStatusIcon = "bgbox_g";                    
-                }else if(vm_status == "Terminated"){
-                    vmStatusIcon = "bgbox_r";                    
-                }else{
-                    vmStatusIcon = "bgbox_g";                    
-                }
-                vm_badge += '<li class="sel_cr ' + vmStatusIcon + '" ><a href="javascript:void(0);" onclick="selectVm(\''+mcisId+'\',\''+vms[o].id+'\')" ><span class="txt">'+vms[o].name+'</span></a></li>';
-                console.log("vm_status : ", vm_status)
+    getCommonMcisData("mcismonitoringmng", mcisId)
+    // var url = "/operation/manages/mcismng/" + mcisId
+    // axios.get(url,{
+    //     headers:{
+    //         // 'Authorization': apiInfo,
+    //         'Content-Type' : "application/json"
+    //     }
+    // }).then(result=>{
+    //     console.log("selectMonitoringMcis result")
+    //     var mcis = result.data.McisInfo
+    //     console.log(mcis)
+    //     var vms = mcis.vm
+    //     console.log(vms)
+    //     var vm_badge ="";
+    //     var vm_options = "";
+    //     var init_vm = vms[0].id
+    //     if(vms){
+    //         vm_len = vms.length
+    //         for(var o in vms){
+    //             var vm_status = vms[o].status
+    //             vm_options +='<option value="'+vms[o].id+'">'+vms[o].name+'|'+vms[o].status+'|'+vms[o].description
+    //
+    //             var vmStatusIcon = "bgbox_b";
+    //             if(vm_status == "Running"){
+    //                 vmStatusIcon = "bgbox_b";
+    //             }else if(vm_status == "include" ){
+    //                 vmStatusIcon = "bgbox_g";
+    //             }else if(vm_status == "Suspended"){
+    //                 vmStatusIcon = "bgbox_g";
+    //             }else if(vm_status == "Terminated"){
+    //                 vmStatusIcon = "bgbox_r";
+    //             }else{
+    //                 vmStatusIcon = "bgbox_g";
+    //             }
+    //             vm_badge += '<li class="sel_cr ' + vmStatusIcon + '" ><a href="javascript:void(0);" onclick="selectVm(\''+mcisId+'\',\''+vms[o].id+'\')" ><span class="txt">'+vms[o].name+'</span></a></li>';
+    //             console.log("vm_status : ", vm_status)
+    //
+    //         }
+    //         var sta = mcis.status
+    //         var sl = sta.split("-");
+    //         var status = sl[0].toLowerCase()
+    //         var mcis_badge = '';
+    //         var mcisStatusIcon = "icon_running_db.png";
+    //         if(status == "running"){
+    //             mcisStatusIcon = 'icon_running_db.png'
+    //         }else if(status == "include" ){
+    //             mcisStatusIcon = 'icon_stop_db.png'
+    //         }else if(status == "suspended"){
+    //             mcisStatusIcon = 'icon_stop_db.png'
+    //         }else if(status == "terminate"){
+    //             mcisStatusIcon = 'icon_terminate_db.png'
+    //         }else{
+    //             mcisStatusIcon = 'icon_stop_db.png'
+    //         }
+    //         mcis_badge = '<img src="/assets/img/contents/' + mcisStatusIcon + '" alt="' + status + '"/> '
+    //
+    //         $("#mcis_info_txt").text("[ "+mcis.name+"("+mcis.id+")"+" ]");
+    //         $("#monitoring_mcis_status_img").empty()
+    //         $("#monitoring_mcis_status_img").append(mcis_badge)
+    //         $("#vmArrList").empty();
+    //         $("#vmArrList").append(vm_badge);
+    //
+    //         // vm list options
+    //         $("#vmList").empty()
+    //         $("#vmList").append(vm_options)
+    //
+    //         $(".ds_cont_mbox .mtbox .g_list .listbox li.sel_cr").each(function(){
+    //             var $sel_list = $(this),
+    //             $detail_view = $(".monitoring_view");
+    //             $sel_list.off("click").click(function(){
+    //                 $sel_list.addClass("active");
+    //                 $sel_list.siblings().removeClass("active");
+    //                 $detail_view.addClass("active");
+    //                 $detail_view.siblings().removeClass("active");
+    //
+    //                 $sel_list.off("click").click(function(){
+    //                     if( $(this).hasClass("active") ) {
+    //                         $sel_list.removeClass("active");
+    //                         $detail_view.removeClass("active");
+    //                     } else {
+    //                             $sel_list.addClass("active");
+    //                             $sel_list.siblings().removeClass("active");
+    //                             $detail_view.addClass("active");
+    //                             $detail_view.siblings().removeClass("active");
+    //                     }
+    //                 });
+    //             });
+    //         });
+    //
+    //     }// end of vms if
+    //
+    // })
+}
 
-            }
-            var sta = mcis.status
-            var sl = sta.split("-");
-            var status = sl[0].toLowerCase()
-            var mcis_badge = '';
-            var mcisStatusIcon = "icon_running_db.png";
-            if(status == "running"){
-                mcisStatusIcon = 'icon_running_db.png'
-            }else if(status == "include" ){
-                mcisStatusIcon = 'icon_stop_db.png'
-            }else if(status == "suspended"){
-                mcisStatusIcon = 'icon_stop_db.png'
-            }else if(status == "terminate"){
-                mcisStatusIcon = 'icon_terminate_db.png'
+function getMcisDataCallbackSuccess(caller, mcisInfo){
+
+        console.log(mcisInfo)
+    var mcisId = mcisInfo.id
+    var vms = mcisInfo.vm
+    console.log(vms)
+    var vm_badge ="";
+    var vm_options = "";
+    var init_vm = vms[0].id
+    if(vms){
+        vm_len = vms.length
+        for(var o in vms){
+            var vm_status = vms[o].status
+            vm_options +='<option value="'+vms[o].id+'">'+vms[o].name+'|'+vms[o].status+'|'+vms[o].description
+
+            var vmStatusIcon = "bgbox_b";
+            if(vm_status == "Running"){
+                vmStatusIcon = "bgbox_b";
+            }else if(vm_status == "include" ){
+                vmStatusIcon = "bgbox_g";
+            }else if(vm_status == "Suspended"){
+                vmStatusIcon = "bgbox_g";
+            }else if(vm_status == "Terminated"){
+                vmStatusIcon = "bgbox_r";
             }else{
-                mcisStatusIcon = 'icon_stop_db.png'
+                vmStatusIcon = "bgbox_g";
             }
-            mcis_badge = '<img src="/assets/img/contents/' + mcisStatusIcon + '" alt="' + status + '"/> '
+            vm_badge += '<li class="sel_cr ' + vmStatusIcon + '" ><a href="javascript:void(0);" onclick="selectVm(\''+mcisId+'\',\''+vms[o].id+'\')" ><span class="txt">'+vms[o].name+'</span></a></li>';
+            console.log("vm_status : ", vm_status)
 
-            $("#mcis_info_txt").text("[ "+mcis.name+"("+mcis.id+")"+" ]");
-            $("#monitoring_mcis_status_img").empty()
-            $("#monitoring_mcis_status_img").append(mcis_badge)
-            $("#vmArrList").empty();
-            $("#vmArrList").append(vm_badge);
+        }
+        var sta = mcisInfo.status
+        var sl = sta.split("-");
+        var status = sl[0].toLowerCase()
+        var mcis_badge = '';
+        var mcisStatusIcon = "icon_running_db.png";
+        if(status == "running"){
+            mcisStatusIcon = 'icon_running_db.png'
+        }else if(status == "include" ){
+            mcisStatusIcon = 'icon_stop_db.png'
+        }else if(status == "suspended"){
+            mcisStatusIcon = 'icon_stop_db.png'
+        }else if(status == "terminate"){
+            mcisStatusIcon = 'icon_terminate_db.png'
+        }else{
+            mcisStatusIcon = 'icon_stop_db.png'
+        }
+        mcis_badge = '<img src="/assets/img/contents/' + mcisStatusIcon + '" alt="' + status + '"/> '
 
-            // vm list options
-            $("#vmList").empty()
-            $("#vmList").append(vm_options)
+        $("#mcis_info_txt").text("[ "+mcisInfo.name+"("+mcisInfo.id+")"+" ]");
+        $("#monitoring_mcis_status_img").empty()
+        $("#monitoring_mcis_status_img").append(mcis_badge)
+        $("#vmArrList").empty();
+        $("#vmArrList").append(vm_badge);
 
-            $(".ds_cont_mbox .mtbox .g_list .listbox li.sel_cr").each(function(){
-                var $sel_list = $(this),
+        // vm list options
+        $("#vmList").empty()
+        $("#vmList").append(vm_options)
+
+        $(".ds_cont_mbox .mtbox .g_list .listbox li.sel_cr").each(function(){
+            var $sel_list = $(this),
                 $detail_view = $(".monitoring_view");
-                $sel_list.off("click").click(function(){
-                    $sel_list.addClass("active");
-                    $sel_list.siblings().removeClass("active");
-                    $detail_view.addClass("active");
-                    $detail_view.siblings().removeClass("active");
+            $sel_list.off("click").click(function(){
+                $sel_list.addClass("active");
+                $sel_list.siblings().removeClass("active");
+                $detail_view.addClass("active");
+                $detail_view.siblings().removeClass("active");
 
-                    $sel_list.off("click").click(function(){
-                        if( $(this).hasClass("active") ) {
-                            $sel_list.removeClass("active");
-                            $detail_view.removeClass("active");
-                        } else {
-                                $sel_list.addClass("active");
-                                $sel_list.siblings().removeClass("active");
-                                $detail_view.addClass("active");
-                                $detail_view.siblings().removeClass("active");
-                        }
-                    });
+                $sel_list.off("click").click(function(){
+                    if( $(this).hasClass("active") ) {
+                        $sel_list.removeClass("active");
+                        $detail_view.removeClass("active");
+                    } else {
+                        $sel_list.addClass("active");
+                        $sel_list.siblings().removeClass("active");
+                        $detail_view.addClass("active");
+                        $detail_view.siblings().removeClass("active");
+                    }
                 });
             });
+        });
 
-        }// end of vms if
-
-    })
+    }// end of vms if
 }
 
 // vm 선택시 해당 vm의 monitoring 조회
